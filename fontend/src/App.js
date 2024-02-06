0569aa5eada63b8400fce0cca019d13157644ecc
astitt1 <astitt@perscholas.org>
2024-02-06 09:15:44 -0500

    used updated version of react router and created product reviews...elispes

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 import { Container } from "react-bootstrap";
 
@@ -11,9 +12,8 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen/>
+          <Outlet/>
         </Container>
-        
       </main>
       <Footer />
     </>
--- fontend/src/index.js
+++ fontend/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import App from './App';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider, //All data router objects are passed to this component to render your app
+} from "react-router-dom";
+import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
 );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
