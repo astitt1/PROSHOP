a265e79718ec8894f84f0da7c1706a774fad2abe
astitt1 <astitt@perscholas.org>
2024-02-06 14:22:20 -0500

    need to add .js to end of imports

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import products from './data/products.js';
 const app = express()
 const port = 5000;
 
@@ -6,6 +7,10 @@ const port = 5000;
 
 app.get('/', (req, res)=>{
     res.send('api...')
+})
+
+app.get('/api/products',( req, res)=>{
+    res.json(products)
 })
 
 app.listen(port, ()=>{
