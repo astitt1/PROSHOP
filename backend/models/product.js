3a2e3c341961b0af42147bfaea6d19bd1025cc7f
astitt1 <astitt@perscholas.org>
2024-02-07 17:24:29 -0500

    added order, product, and user schema

---
+++ backend/models/product.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      //the user leaving a review
+      type: Moncoose.Schema.Types.ObjectId,
+      required: true,
+      ref: User, //user collection
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: { type: Number, required: true },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      //admin user
+      type: Moncoose.Schema.Types.ObjectId,
+      required: true,
+      ref: User, //user collection
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema], //1-many relationship
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
